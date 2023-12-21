42ac2f549e93d2f1fd70ec469d146fd469af9522
Kilar07 <kpafifov@bk.ru>
2023-12-21 12:53:33 +0300

    Toggle favorite book

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -1,58 +1,72 @@
-import React, {useState} from 'react'
-import { useDispatch } from 'react-redux'
-import {v4 as uuidv4} from 'uuid'
-import { addBook } from '../../redux/books/actionCreators'
-import booksData from '../../data/books.json'
-import './BookForm.css'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { v4 as uuidv4 } from "uuid";
+import { addBook } from "../../redux/books/actionCreators";
+import booksData from "../../data/books.json";
+import "./BookForm.css";
 
 const BookForm = () => {
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
-  const dispatch = useDispatch()
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const dispatch = useDispatch();
 
-  const handleAddRandomBook = () =>{
-    const randomIndex = Math.floor(Math.random() * booksData.length)
-    const randomBook = booksData[randomIndex]
+  const handleAddRandomBook = () => {
+    const randomIndex = Math.floor(Math.random() * booksData.length);
+    const randomBook = booksData[randomIndex];
     const randomBookWithId = {
       ...randomBook,
-      id: uuidv4()
-    }
+      id: uuidv4(),
+      isFavorite: false,
+    };
 
-    dispatch(addBook(randomBookWithId))
-  }
+    dispatch(addBook(randomBookWithId));
+  };
 
-  const handleSubmit = (e) =>{
-    e.preventDefault()
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-    if(title && author){
+    if (title && author) {
       const book = {
         title,
         author,
-        id: uuidv4()
-      }
-      dispatch(addBook(book))
-      setTitle('')
-      setAuthor('')
+        id: uuidv4(),
+        isFavorite: false,
+      };
+      dispatch(addBook(book));
+      setTitle("");
+      setAuthor("");
     }
-  }
+  };
 
   return (
-    <div className='app-block book-form'>
+    <div className="app-block book-form">
       <h2>Add a New Book</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor='title'>Title: </label>
-          <input type='text' id='title' value={title} onChange={(e) => setTitle(e.target.value)} />
+          <label htmlFor="title">Title: </label>
+          <input
+            type="text"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <div>
-          <label htmlFor='author'>Author: </label>
-          <input type='text' id='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <label htmlFor="author">Author: </label>
+          <input
+            type="text"
+            id="author"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          />
         </div>
-        <button type='submit'>Add Book</button>
-        <button type='button' onClick={handleAddRandomBook}>Add Random</button>
+        <button type="submit">Add Book</button>
+        <button type="button" onClick={handleAddRandomBook}>
+          Add Random
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default BookForm
+export default BookForm;
