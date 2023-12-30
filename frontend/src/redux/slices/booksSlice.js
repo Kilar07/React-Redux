34b36bd3272b3b3803e4f306224f0d547582e6bd
Kilar07 <kpafifov@bk.ru>
2023-12-30 12:04:32 +0300

    Added createAsyncThunk

--- frontend/src/redux/slices/booksSlice.js
+++ frontend/src/redux/slices/booksSlice.js
@@ -1,8 +1,13 @@
-import axios from 'axios'
-import { createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import createBookWithID from "../../utils/createBookWithID";
 
 const initialState = [];
+
+export const fetchBook = createAsyncThunk("books/fetchBook", async () => {
+  const res = await axios.get("http://localhost:4000/random-book");
+  return res.data;
+});
 
 const booksSlice = createSlice({
   name: "books",
@@ -15,9 +20,9 @@ const booksSlice = createSlice({
       return state.filter((item) => item.id !== action.payload);
     },
     toggleFavorite: (state, action) => {
-      state.forEach(book => {
-        if(book.id === action.payload){
-          book.isFavorite = !book.isFavorite
+      state.forEach((book) => {
+        if (book.id === action.payload) {
+          book.isFavorite = !book.isFavorite;
         }
       });
       // return state.map((book) =>
@@ -25,23 +30,19 @@ const booksSlice = createSlice({
       //     ? { ...book, isFavorite: !book.isFavorite }
       //     : book
       // );
-    }
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchBook.fulfilled, (state, action) => {
+      if (action?.payload?.title && action?.payload?.author) {
+        state.push(createBookWithID(action.payload, "API"));
+      }
+    });
   },
 });
 
-export const {addBook, deleteBook, toggleFavorite} = booksSlice.actions
+export const { addBook, deleteBook, toggleFavorite } = booksSlice.actions;
 
-export const thunkFunction = async (dispatch, getState) => {
-  try {
-    const res = await axios.get("http://localhost:4000/random-book")
-  if (res?.data?.title && res?.data?.author){
-    dispatch(addBook(createBookWithID(res.data, 'API')))
-  }
-  } catch (error) {
-    console.log('Error fetching random book', error)
-  }
-}
+export const selectBooks = (state) => state.books;
 
-export const selectBooks = (state) => state.books
-
-export default booksSlice.reducer
+export default booksSlice.reducer;
